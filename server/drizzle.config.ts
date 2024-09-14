72466af1898801486fd22d0799983c22e9a2e539
Vinicius Girotto <vinicius.s.carvalho23@gmail.com>
2024-09-14 13:27:39 -0300

    subingo projeto

---
+++ server/drizzle.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'drizzle-kit'
+export default defineConfig({
+  schema: './schema.ts',
+  out: './.migrations',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: 'postgresql://docker:docker@localhost:5432/inorbit',
+  },
+})
